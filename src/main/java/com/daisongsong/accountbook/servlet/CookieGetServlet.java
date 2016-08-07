55dee4ac816b87f61c8a1649711331b2f19afed1
daisongsong <daisongsong@daisongsongdeMac-mini.local>
2016-08-07 11:55:01 +0800

    change method to post

--- src/main/java/com/daisongsong/accountbook/servlet/CookieGetServlet.java
+++ src/main/java/com/daisongsong/accountbook/servlet/CookieGetServlet.java
@@ -16,7 +16,8 @@ import java.util.Map;
 public class CookieGetServlet extends HttpServlet {
 
     @Override
-    protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+    protected void doPost(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        req.setCharacterEncoding("UTF-8");
         resp.setCharacterEncoding("UTF-8");
         printCookies(req, resp);
 
